e8c4ce91d1599de5086958f5be6f0e4e1274e875
Fespinola1 <tecnico_18@Outlook.es>
2016-07-19 02:04:06 -0500

    Usando XAML en Xamarin Forms

---
+++ XamarinFormsXAML/XamarinFormsXAML/XamarinFormsXAML/HomePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsXAML
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        //agregamos la variable
+
+        int count = 0;
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //se ejecuta cuando la aplicación aparece
+
+            HDCsumar.Clicked += HDCsumar_Clicked;
+        }
+
+        // el evento que creamos
+        private void HDCsumar_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            HDCsumar.Text = count.ToString();
+        }
+    }
+}
